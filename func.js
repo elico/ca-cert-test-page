a802bde371a84b52840f2cf54325fb3e2a9d00ae
Eliezer Croitoru <eliezer@ngtech.co.il>
2017-01-29 00:42:13 +0200

    Changed the logi due to some android devices(LG-Dxxx) not being tested properly.

--- func.js
+++ func.js
@@ -2,9 +2,11 @@ function processCaTestRequest(e) {
     document.getElementById("catest").innerText = "STARTED CA TEST";
     if (caTest.readyState == 4 && caTest.status == 200) {
         document.getElementById("catest").innerText = "Finished CA TEST SUCCESFULLY";
+        document.getElementById("test-img").innerHTML = '<img id="result" src="https://moodle.ngtech.co.il/ca-test/success.gif" style="max-width: 130px; display: block; margin: 0 auto">'
+        a
     } else {
         document.getElementById("catest").innerText = "ERROR on CA TEST";
-        document.getElementById("result").src = "http://moodle.ngtech.co.il/ca-test/failure.gif";
+        document.getElementById("test-img").innerHTML = '<img id="result" src="http://moodle.ngtech.co.il/ca-test/failure.jpg" style="max-width: 130px; display: block; margin: 0 auto">';
 
     }
 }
@@ -13,18 +15,15 @@ function processCaTestRequest(e) {
 function processNetRequest(e) {
     if (netTest.readyState == 4 && netTest.status == 200) {
 
-        //        var response = JSON.parse(xhr.responseText);
+        // var response = JSON.parse(xhr.responseText);
 
-        //        alert(response.ip);
-        //        alert(response.date);
-        //      setInterval(function(){document.getElementById("nettest").innerText = "Finished NET TEST"; },3000);
+        // alert(response.ip);
+        // alert(response.date);
+        // setInterval(function(){document.getElementById("nettest").innerText = "Finished NET TEST"; },3000);
         document.getElementById("nettest").innerText = "Finished NET TEST SUCCESFULLY";
-        caTest.send(encodeURI('date=' + d.toLocaleString()));
 
         caTest.onreadystatechange = processCaTestRequest;
-
-
-
+        caTest.send(encodeURI('date=' + d.toLocaleString()));
     } else {
         document.getElementById("nettest").innerText = "ERROR on NET TEST";
         document.getElementById("result").src = "";
@@ -46,6 +45,6 @@ caTest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 netTest.onreadystatechange = processNetRequest;
 
 function testSSL() {
-    document.getElementById("result").src = "http://moodle.ngtech.co.il/ca-test/Loading2.gif";
+    document.getElementById("test-img").innerHTML = '<img id="result" src="http://moodle.ngtech.co.il/ca-test/Loading2.gif" style="max-width: 130px; display: block; margin: 0 auto">'
     netTest.send(encodeURI('date=' + d.toLocaleString()));
 }
